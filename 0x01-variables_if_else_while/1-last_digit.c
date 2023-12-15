9205ccbf08307f27c2f4384c2ce6ad53180ceb9e
= <Soulmate-00>
2023-12-15 21:49:02 +0100

    adding last_digit C program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,8 +8,7 @@
  */
 int main(void)
 {
-	int n;
-	int x;
+	int n, x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -19,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d and is great then 5\n", n, x);
 	else if (x == 0)
 		printf("Last digit of %d is %d and is 0\n", n, x);
-	else (x < 6 && x != 0)
+	else if  (x < 6 && x != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	return (0);
 }
