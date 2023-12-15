7f9844ab9e936edab35b21bc4e29d0d0c02de978
= <Soulmate-00>
2023-12-15 02:25:00 +0100

    adding printf C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Description: C program that prints with printf function
+ *
+ * Return: always 0
+ */
+
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return (0);
+}
