8614ac5641ffcc57ed187990bb4bd10f2d396d6a
= <Soulmate-00>
2023-12-15 23:53:10 +0100

    adding task06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point main function
+ *
+ *Return: Always 0
+*/
+int main(void)
+{
+	int lr;
+
+	for (lr = '0'; lr <= '9'; lr++)
+		putchar(lr);
+	putchar('\n');
+
+	return (0);
+}
