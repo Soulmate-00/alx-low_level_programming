40f42cc531a4af7c4e07a7da79dd63cf44b5f853
= <Soulmate-00>
2023-12-15 22:16:29 +0100

    adding last_digit C program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,21 +5,22 @@
  * main - Entry point main function
  *
  * Return: always 0
- */
+*/
 int main(void)
 {
-	int n, x;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	x = n % 10;
-	if (x > 5)
-		printf("Last digit of %d is %d and is great then 5", n, x);
-	else if (x == 0)
-		printf("Last digit of %d is %d and is 0", n, x);
-	else if  (x < 6 && x != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, x);
-	printf("\n");
+	if ((n % 10) > 5)
+	{
+			printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+
 	return (0);
 }
