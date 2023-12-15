dfab8291c7fb8df1eef2c85c54b46679409ae726
= <Soulmate-00>
2023-12-15 23:18:18 +0100

    adding task02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point main function
+ *
+ *Return: Always 0
+*/
+int main(void)
+{
+	char lr;
+
+	for (lr = 'a'; lr <= 'z'; lr++)
+		putchar(lr);
+	putchar('\n');
+
+	return (0);
+}
