0402757e6fbaf38f26be731900178fe9386192a5
= <Soulmate-00>
2023-12-15 22:03:30 +0100

    adding last_digit C program

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,11 @@ int main(void)
 	/* your code goes there */
 	x = n % 10;
 	if (x > 5)
-		printf("Last digit of %d is %d and is great then 5\n", n, x);
+		printf("Last digit of %d is %d and is great then 5", n, x);
 	else if (x == 0)
-		printf("Last digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0", n, x);
 	else if  (x < 6 && x != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, x);
+	printf("\n");
 	return (0);
 }
