5256d20779da9b586e8a2f637f09d39154bfa141
= <Soulmate-00>
2023-12-15 01:17:38 +0100

    name script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int	main(void)
+{
+    char str0[] = "Programming is like building a multilingual puzzle";
+    
+    puts(str0); 
+    
+    return 0;
+
+
+}
