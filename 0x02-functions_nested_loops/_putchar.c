1dc140f7acdb6b05e74ec9b47b3d16e4df52b156
= <=>
2023-12-20 00:54:57 +0100

    task0 def _putchar function

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - write the character x to stdout
- * @x the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return 0 success / -1 error
-*/
-
-int	_putchar(char x)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	return (write(1, &x, 1));
-
+	return (write(1, &c, 1));
+}
