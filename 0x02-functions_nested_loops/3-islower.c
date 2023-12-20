31a5531e907c9b44c7e859a2933e1faf3a54eeb8
= <=>
2023-12-20 20:16:01 +0100

    task03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- *
  * _islower - function that checks for lowercase character
  *
+ * @c: The character to print
+ *
  * Return: 1 if c is lowercase, 0 otherwise
- */
+ *
+*/
 
-int _islower(int c)
+int	_islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
