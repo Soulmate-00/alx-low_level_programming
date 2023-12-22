60ea07dfbaae4437ca58a9c0e563a7c62ac0bee4
= <=>
2023-12-23 00:30:55 +0100

    task03

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: always 0
+ **/
+
+void	print_numbers(void)
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		_putchar(z + '0');
+	}
+	_putchar('\n');
+}
