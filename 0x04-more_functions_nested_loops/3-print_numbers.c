905f932b79bee2d293c1053fb66f531088c12824
= <=>
2023-12-23 00:57:33 +0100

    task03

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,7 @@
  * print_numbers - function that prints the numbers, from 0 to 9
  *
  * Return: always 0
- **/
+*/
 
 void	print_numbers(void)
 {
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Description: prints numbers from 0 to 9 witout 2,4 numbers
+ *
+ * Return: run numbers from 0 to 9
+ *
+*/
+
+void	print_most_numbers(void)
+{
+	int z = 0;
+
+	for (; z <= 9; z++)
+	{
+		if (z == 2 || z == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putcar(z + '0');
+		}
+		_putcar('\n')
+	}
+}
