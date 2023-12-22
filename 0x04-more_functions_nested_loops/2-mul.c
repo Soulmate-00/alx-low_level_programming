383946991a5708f7c4ac249d6baf01b503ba544d
= <=>
2023-12-22 23:26:06 +0100

    task02

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point function that multiplies two integers
+ * @a: te first number to multiply
+ * @b: the second number to multiplay with a
+ * Return: always 0
+*/
+
+int	mul(int a, int b)
+{
+	return (a * b);
+}
