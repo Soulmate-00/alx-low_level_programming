53bd53f02bbfdf53be2e03c7c24894e219a1f670
= <=>
2023-12-25 00:30:26 +0100

    task07

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-int _putchar(char c);
-void print_alphabet(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int x);
+int	_putchar(char c);
+void	print_alphabet(void);
+int	_islower(int c);
+int	_isalpha(int c);
+int	print_sign(int n);
+int	_abs(int);
+int	print_last_digit(int);
