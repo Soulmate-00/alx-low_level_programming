c5043bb39aaaa65ae78b40c3ecf254b7111b9568
= <=>
2023-12-24 21:42:47 +0100

    adding task06

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ *
+ * @n : the parameter
+ *
+ * Return : always 0
+*/
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		_putchar('-');
+	}
+	_putchar('\n');
+}
