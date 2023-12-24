de2e59759048d58daedcd4ff9a3e0df18d0e40c5
= <=>
2023-12-24 22:14:11 +0100

    addinng task07

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  function that prints the last digit of a number
+ *
+ * @x:the parameter
+ *
+ * Return: always 0
+ *
+*/
+
+int	print_last_digit(int x)
+{
+	int lastdigit = x % 10;
+
+	if (lastdigit < 0)
+	lastdigit *= -1;
+
+	_putchar (lastdigit < 0);
+	return (lastdigit);
+}
