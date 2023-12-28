672b4ef978b41120788dacf0ebf4624922f05a1e
= <=>
2023-12-28 20:53:29 +0100

    adding task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @x:first integer
+ * @y: second integer
+ *
+ * Return: the sum of x and y
+ */
+int	add(int x, int y)
+{
+	return (x + y);
+}
