67cc477f526edf90bec2dce6bdb757773f7902ee
= <=>
2024-01-03 03:39:13 +0100

    adding task02

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: The parameter to input
+ *
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s = '\0'; s++)
+		++count;
+
+	return (count);
+}
